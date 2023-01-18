d24a52c4a87cbc5687dc8004db3e91b2a973873d
Abayomi Aluko <franicons@gmail.com>
2023-01-18 14:01:43 +0100

    feat: project preparation
    * install required package needed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],  theme: {
     extend: {},
   },
   plugins: [],
