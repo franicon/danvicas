5d9daef4f42e115a728f9b92fc45c204fada534e
Abayomi Aluko <franicons@gmail.com>
2023-01-18 14:14:33 +0100

    feat: project preparation
    * install required package needed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Danvicas</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
-    </style>
-    @vite('resources/css/app.css')
-</head>
-    <body>
-        <h3 class="text-3xl font-black">Hello world</h3>
-    </body>
-</html>
+@extends('layouts.public-layout')
+@section('content')
+    <h3 class="text-3xl font-black">Hello world</h3>
+@endsection
