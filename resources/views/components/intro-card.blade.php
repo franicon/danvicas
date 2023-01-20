da69fa3cef0c79df3c186cc346b15fab8b4d2802
Abayomi Aluko <franicons@gmail.com>
2023-01-20 15:42:51 +0100

    feat: Building landing page
    * install required package needed

--- resources/views/components/intro-card.blade.php
+++ resources/views/components/intro-card.blade.php
@@ -1,19 +1,21 @@
 <section>
-    <div class="tw-flex tw-flex-col tw-my-24 justify-content-between align-items-center lg:tw-flex-row">
-        <div class="md:tw-px-5 px-4 py-5 md:tw-relative tw-absolute bg-white tw-bg-opacity-10">
-            <div class="tw-text-3xl font-bold tw-w-96 tw-content-end tw-font-bold">WE ARE EXPERT IN CONSTRUCTION FIELD</div>
+    <div class="tw-flex tw-flex-col tw-my-24 justify-content-between align-items-center
+    md:tw-flex-row tw-relative tw-overflow-hidden">
+        <div class="sm:tw-px-10 px-4 md:tw-py-5 tw-text-stone-700 tw-max-w-xl lg:tw-max-w-full">
+            <div class="tw-text-3xl font-bold tw-w-96 tw-content-end tw-font-black">WE ARE EXPERT IN CONSTRUCTION FIELD</div>
             <div class="tw-border tw-border-[1.9px] tw-w-16  my-5 tw-border-blue-500"></div>
-            <p class="tw-text-xl mb-5">Our outfit is well equipped to handle any aspect of Building
+            <p class=" tw-text-lg lg:tw-text-xl mb-5">Our outfit is well equipped to handle any aspect of Building
                 and Civil project(s) from designing to commissioning and has gained
                 appreciable relationship with related partners within and Nigeria.</p>
-            <p class="tw-text-xl mb-2">
+            <p class="tw-text-lg lg:tw-text-xl mb-5">
                 The firm’s optimal approach to meeting client’s satisfactions is out
                 of wide range of experience built up over the years in numerous
                 construction and design services, rendered to individuals,
                 industries, corporate bodies, and government organizations.</p>
         </div>
+{{--        Section Image--}}
         <div class="">
-            <img src="/img/home-intro.png" alt="" class="tw-max-w-xl">
+            <img src="/img/home-intro.png" alt="" class="lg:tw-max-w-xl tw-max-w-lg">
         </div>
     </div>
 </section>
