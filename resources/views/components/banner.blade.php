6da53d2c13e91fef9577192910bbdfba68475e5e
Abayomi Aluko <franicons@gmail.com>
2023-01-20 11:40:52 +0100

    feat: Building landing page
    * install required package needed

---
+++ resources/views/components/banner.blade.php
@@ -0,0 +1,47 @@
+<div id="carouselExampleDark" class="carousel carousel-dark slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
+                aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active " data-bs-interval="10000">
+            <img src="./img/banner-2.png" class="sm:tw-flex w-100 tw-hidden" alt="...">
+            <img src="./img/sm.jpg" class="w-100 sm:tw-hidden" alt="...">
+            <div
+                class="carousel-caption tw-top-40 sm:tw-top-16 md:tw-top-20 tw-top-4
+                lg:tw-top-32 xl:tw-top-64 md:tw-max-w-4xl  tw-max-w-full mx-auto text-light">
+                <div
+                    class="xl:tw-text-6xl lg:tw-text-5xl md:tw-text-4xl
+                    sm:tw-text-3xl tw-text-xl tw-font-black tw-leading-normal mb-3">
+                    WE ARE LEADING THE WAY
+                    CONSTRUCTION WORKS
+                </div>
+                <div class="btn btn-primary px-4 py-2 tw-font-black">Get Started</div>
+            </div>
+        </div>
+        <div class="carousel-item" data-bs-interval="2000">
+            <img src="./img/banner-1.png" class="w-100 sm:tw-flex w-100 tw-hidden" alt="...">
+            <img src="./img/sm-2.jpg" class="w-100 sm:tw-hidden" alt="...">
+            <div
+                class="carousel-caption tw-top-10 sm:tw-top-16 md:tw-top-20 tw-top-4 lg:tw-top-32 xl:tw-top-64 md:tw-max-w-4xl  tw-max-w-full mx-auto text-light">
+                <div
+                    class="xl:tw-text-6xl lg:tw-text-5xl md:tw-text-4xl sm:tw-text-3xl tw-text-xl tw-font-black tw-leading-7 sm:mb-4">
+                    WE ARE LEADING THE WAY
+                    CONSTRUCTION WORKS
+                </div>
+                <div class="btn btn-primary px-5 py-2 tw-font-black">Get Started</div>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev d-none" type="button" data-bs-target="#carouselExampleDark"
+            data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next d-none" type="button" data-bs-target="#carouselExampleDark"
+            data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
