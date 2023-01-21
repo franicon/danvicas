61b9ad5cae721942e8bd836091491eb48cfb3df6
Abayomi Aluko <franicons@gmail.com>
2023-01-21 14:13:34 +0100

    feat: Building landing page

--- resources/views/components/recent-project.blade.php
+++ resources/views/components/recent-project.blade.php
@@ -10,24 +10,17 @@
             </div>
             <div class="btn btn-primary px-4 py-2 tw-font-black mt-4">See all Project</div>
         </div>
-        <div class="tw-grid tw-gap-4 lg:tw-grid-cols-3 md:tw-grid-cols-2 sm:tw-grid-cols-2  tw-my-20">
-            <div class="">
+        <div class="tw-grid tw-gap-0 lg:tw-grid-cols-3 md:tw-grid-cols-2 sm:tw-grid-cols-2  tw-my-20">
+            <div class="tw-relative overflow-hidden tw-cursor-pointer">
                 <img src="/img/project-1.png" alt="" class="">
-            </div>
-            <div class="">
-                <img src="/img/project-2.png" alt="" class="">
-            </div>
-            <div class="">
-                <img src="/img/project-3.png" alt="" class="">
-            </div>
-            <div class="">
-                <img src="/img/project-2.png" alt="" class="">
-            </div>
-            <div class="">
-                <img src="/img/project-1.png" alt="" class="">
-            </div>
-            <div class="">
-                <img src="/img/project-3.png" alt="" class="">
+                <div class="project-overlay">
+                    <div class="tw-w-full tw-flex tw-flex-col tw-items-center tw-justify-center">
+                        <p class="md:tw-text-xl tw-text-[15px]">Building Construction</p>
+                        <div class="tw-cursor-pointer">
+                            <a href="/services"><img src="/img/download.svg" alt="" class="tw-w-10 tw-fill-amber-50"></a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
