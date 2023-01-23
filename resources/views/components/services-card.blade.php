5a8fa80b4d7843068955cc3ed5dc14ff3eb05d7a
Abayomi Aluko <franicons@gmail.com>
2023-01-23 12:19:18 +0100

    feat: Building Service page

--- resources/views/components/services-card.blade.php
+++ resources/views/components/services-card.blade.php
@@ -10,21 +10,21 @@
     </div>
     <div class="tw-grid tw-gap-4 tw-grid-cols-1 md:tw-grid-cols-2 lg:tw-grid-cols-3 tw-mt-20 text-center">
         <div class="tw-my-12 px-4">
-            <div class="service-card ">
+            <div class="service-card tw-border-b-white">
                 <img src="/img/vector.png" alt="" class="tw-w-20 mx-auto tw-mb-4">
                 <h1 class="tw-text-2xl">Logistics</h1>
                 <p class="md:tw-text-[16px] tw-text-[15px] ">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
             </div>
         </div>
         <div class="tw-my-12 px-4">
-            <div class="service-card ">
+            <div class="service-card tw-border-b-white">
                 <img src="/img/vector.png" alt="" class="tw-w-20 mx-auto tw-mb-4">
                 <h1 class="tw-text-2xl">Logistics</h1>
                 <p class="md:tw-text-[16px] tw-text-[15px] ">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
             </div>
         </div>
         <div class="tw-my-12 px-4">
-            <div class="service-card ">
+            <div class="service-card tw-border-b-white">
                 <img src="/img/vector.png" alt="" class="tw-w-20 mx-auto tw-mb-4">
                 <h1 class="tw-text-2xl">Logistics</h1>
                 <p class="md:tw-text-[16px] tw-text-[15px] ">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,4 +5,8 @@
             <div class="tw-text-6xl  ">Contact Us</div>
         </x-slot:head>
     </x-small-banner>
+    <div class="tw-max-w-7xl mx-auto tw-my-12">
+        {{--        Instro Section--}}
+        <x-contact-form/>
+    </div>
 @endsection
