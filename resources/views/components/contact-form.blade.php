422f2303be25414ba1893f9e4ba06b330468fa95
Abayomi Aluko <franicons@gmail.com>
2023-01-23 17:43:50 +0100

    feat: Building Service page

--- resources/views/components/contact-form.blade.php
+++ resources/views/components/contact-form.blade.php
@@ -1,14 +1,14 @@
-<section class="container tw-pb-28">
-    <form class="row g-3">
+<section class="container tw-py-10">
+    <form class="row g-3 mt-5">
         <div class="col-md-6">
            <div class="px-3">
-               <div class="my-3">
-                   <label for="inputEmail4" class="form-label">Name/Company</label>
-                   <input type="email" class="form-control p-3" id="inputEmail4">
+               <div class="form-floating mb-4">
+                   <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                   <label for="floatingInput">Email address</label>
                </div>
-               <div class="my-3">
-                   <label for="inputEmail4" class="form-label">Email</label>
-                   <input type="email" class="form-control p-3" id="inputEmail4">
+               <div class="form-floating mb-4">
+                   <input type="text" class="form-control" id="floatingPassword" placeholder="company name">
+                   <label for="floatingPassword">Company Name</label>
                </div>
                <div class="form-floating">
                    <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
@@ -20,7 +20,7 @@
             </div>
         </div>
         <div class="col-md-6">
-           <div class="px-3 tw-space-y-8 mt-5">
+           <div class="px-3 tw-space-y-8">
                <div class="">
                    <div class="tw-font-bold tw-text-2xl">Address:</div>
                    <div class="md:tw-text-xl tw-text-md tw-py-2">4, Agba-oku street, off Awosika bus-stop, Ikeja, Lagos</div>
