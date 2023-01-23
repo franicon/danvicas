6a3f195730a91bd51a2b47d25f7956d80893b791
Abayomi Aluko <franicons@gmail.com>
2023-01-23 09:03:19 +0100

    feat: Building Service page

--- resources/views/components/double-card.blade.php
+++ resources/views/components/double-card.blade.php
@@ -1,8 +1,8 @@
-    <section class="tw-pb-16 tw-pt-10 ">
-        <div class="tw-flex tw-flex-row-justify-between tw-items-center">
-            <div class="tw-px-4 tw-max-w-xl mx-auto tw-px-4">
+    <section class="tw-pt-24  md:tw-space-y-20">
+        <div class="tw-flex tw-flex-col lg:tw-flex-row tw-justify-between tw-items-center service">
+            <div class="mx-auto lg:tw-pr-20 lg:tw-px-0 tw-px-6 tw-mb-6">
                 <div class="tw-text-3xl tw-font-black tw-pb-6">CIVIL</div>
-                <div class="tw-text-[17px] tw-leading-loose">Lorem ipsum dolor sit amet consectetur. Arcu eu pellentesque mauris tincidunt
+                <div class="md:tw-text-[17px] tw-text-[15px] tw-leading-loose">Lorem ipsum dolor sit amet consectetur. Arcu eu pellentesque mauris tincidunt
                     in accumsan. Facilisis non pharetra ante convallis eleifend at molestie elementum
                     Lorem ipsum dolor sit amet consectetur. Arcu eu pellentesque mauris tincidunt in
                     accumsan. Facilisis non pharetra ante convallis eleifend at molestie elementum Lorem
@@ -10,11 +10,11 @@
                     Facilisis non pharetra ante convallis eleifend at molestie elementum Lorem ipsum dolor sit.
                     accumsan. Facilisis non pharetra ante convallis eleifend at molestie elementum Lorem
                     ipsum dolor sit amet consectetur. Arcu eu pellentesque mauris tincidunt in accumsan.
-                    Facilisis non pharetra ante convallis eleifend at molestie elementum Lorem ipsum dolor sit.
+                    Facilisis non pharetra ante convallis eleifend at molestie elementum Lorem ipsum dolorsit.
                 </div>
             </div>
             <div class="">
-                <img src="img/service-1.png" alt="" class="lg:tw-max-w-xl sm:tw-max-w-lg tw-max-w-full">
+                <img src="img/civil.jpg" alt="" class="lg:tw-max-w-xl sm:tw-max-w-lg tw-max-w-full">
             </div>
         </div>
     </section>
