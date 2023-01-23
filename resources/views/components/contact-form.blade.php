a1be7bc2aebdac891ef0b7f657a3627ea6fb069e
Abayomi Aluko <franicons@gmail.com>
2023-01-23 17:38:51 +0100

    feat: Building Service page

--- resources/views/components/contact-form.blade.php
+++ resources/views/components/contact-form.blade.php
@@ -1,38 +1,39 @@
-<section class="">
+<section class="container tw-pb-28">
     <form class="row g-3">
         <div class="col-md-6">
-            <label for="inputEmail4" class="form-label">Email</label>
-            <input type="email" class="form-control" id="inputEmail4">
+           <div class="px-3">
+               <div class="my-3">
+                   <label for="inputEmail4" class="form-label">Name/Company</label>
+                   <input type="email" class="form-control p-3" id="inputEmail4">
+               </div>
+               <div class="my-3">
+                   <label for="inputEmail4" class="form-label">Email</label>
+                   <input type="email" class="form-control p-3" id="inputEmail4">
+               </div>
+               <div class="form-floating">
+                   <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+                   <label for="floatingTextarea2">Message</label>
+               </div>
+           </div>
+            <div class="col-12 mt-4 px-3">
+                <button type="submit" class="btn btn-primary px-5 py-2">Send Message</button>
+            </div>
         </div>
         <div class="col-md-6">
-            <label for="inputPassword4" class="form-label">Password</label>
-            <input type="password" class="form-control" id="inputPassword4">
-        </div>
-        <div class="col-12">
-            <label for="inputAddress" class="form-label">Address</label>
-            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-        </div>
-        <div class="col-12">
-            <label for="inputAddress2" class="form-label">Address 2</label>
-            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-        </div>
-        <div class="col-md-6">
-            <label for="inputCity" class="form-label">City</label>
-            <input type="text" class="form-control" id="inputCity">
-        </div>
-        <div class="col-md-4">
-            <label for="inputState" class="form-label">State</label>
-            <select id="inputState" class="form-select">
-                <option selected>Choose...</option>
-                <option>...</option>
-            </select>
-        </div>
-        <div class="col-md-2">
-            <label for="inputZip" class="form-label">Zip</label>
-            <input type="text" class="form-control" id="inputZip">
-        </div>
-        <div class="col-12">
-            <button type="submit" class="btn btn-primary px-5 py-2">Send Message</button>
+           <div class="px-3 tw-space-y-8 mt-5">
+               <div class="">
+                   <div class="tw-font-bold tw-text-2xl">Address:</div>
+                   <div class="md:tw-text-xl tw-text-md tw-py-2">4, Agba-oku street, off Awosika bus-stop, Ikeja, Lagos</div>
+               </div>
+               <div class="">
+                   <div class="tw-font-bold tw-text-2xl">Email Us At:</div>
+                   <div class="md:tw-text-xl tw-text-md tw-py-2">info@danvicas.com</div>
+               </div>
+               <div class="">
+                   <div class="tw-font-bold tw-text-2xl">Contact-us:</div>
+                   <div class="md:tw-text-xl tw-text-md tw-py-2">+234 806 283 188</div>
+               </div>
+           </div>
         </div>
     </form>
 </section>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,11 +2,12 @@
 @section('content')
     <x-small-banner>
         <x-slot:head>
-            <div class="tw-text-6xl  ">Contact Us</div>
+            <div class="md:tw-text-6xl tw-text-4xl">Contact Us</div>
         </x-slot:head>
     </x-small-banner>
     <div class="tw-max-w-7xl mx-auto tw-my-12">
         {{--        Instro Section--}}
         <x-contact-form/>
+        <x-testimonials/>
     </div>
 @endsection
