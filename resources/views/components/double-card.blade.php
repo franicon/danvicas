301b39f3e095008f5e6c654b15d611c9e04a45c5
Abayomi Aluko <franicons@gmail.com>
2023-01-23 09:26:02 +0100

    feat: Building Service page

--- resources/views/components/double-card.blade.php
+++ resources/views/components/double-card.blade.php
@@ -1,8 +1,9 @@
     <section class="tw-pt-24  md:tw-space-y-20">
-        <div class="tw-flex tw-flex-col lg:tw-flex-row tw-justify-between tw-items-center service">
-            <div class="mx-auto lg:tw-pr-20 lg:tw-px-0 tw-px-6 tw-mb-6">
+        <div class="d-card service">
+            <div class="d-card-text">
                 <div class="tw-text-3xl tw-font-black tw-pb-6">CIVIL</div>
-                <div class="md:tw-text-[17px] tw-text-[15px] tw-leading-loose">Lorem ipsum dolor sit amet consectetur. Arcu eu pellentesque mauris tincidunt
+                <div class="d-card-main-text">
+                    Lorem ipsum dolor sit amet consectetur. Arcu eu pellentesque mauris tincidunt
                     in accumsan. Facilisis non pharetra ante convallis eleifend at molestie elementum
                     Lorem ipsum dolor sit amet consectetur. Arcu eu pellentesque mauris tincidunt in
                     accumsan. Facilisis non pharetra ante convallis eleifend at molestie elementum Lorem
@@ -14,7 +15,7 @@
                 </div>
             </div>
             <div class="">
-                <img src="img/civil.jpg" alt="" class="lg:tw-max-w-xl sm:tw-max-w-lg tw-max-w-full">
+                <img src="img/civil.jpg" alt="" class="d-card-img">
             </div>
         </div>
     </section>
