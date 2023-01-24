4d3ee509fd2f7885f4b79e6493075341dd3be878
Abayomi Aluko <franicons@gmail.com>
2023-01-24 23:57:58 +0100

    feat: Building Service page

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ProjectController extends Controller
-{
-    //
+class ProjectController extends Controller {
+    public function index() {
+
+    }
 }
