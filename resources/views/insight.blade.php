0ff3acfe68d4ab271ad8fc9310d4c1742a16cdae
Abayomi Aluko <franicons@gmail.com>
2023-01-24 10:58:00 +0100

    feat: Building Service page

--- resources/views/insight.blade.php
+++ resources/views/insight.blade.php
@@ -2,88 +2,42 @@
 @section('content')
     <x-big-banner>
         <x-slot:head>
-            <div class="md:tw-text-6xl tw-text-4xl">insight</div>
+            <div class="md:tw-text-6xl tw-text-4xl">Insight-Details</div>
         </x-slot:head>
     </x-big-banner>
     <div class="tw-max-w-7xl mx-auto tw-py-16">
-        <article class="tw-mb-24 px-3">
-            <div class="tw-max-w-3xl mx-auto text-center">
-                <div class="c-header tw-font-black"> More insight</div>
-                <div class="c-border mx-auto"></div>
-                <div class="md:tw-text-xl tw-text-[15px] tw-text-gray-500">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Commodi unde impedit, necessitatibus, soluta sit quam minima
-                    expedita atque corrupti reiciendis.
+        <div class="tw-flex lg:tw-flex-row tw-flex-col tw-justify-center lg:tw-items-start tw-items-center">
+            <div class="lg:tw-px-6 xl:tw-px-10">
+                <img src="/img/post-1.png" alt="" class="tw-rounded-3xl">
+                <p class="tw-font-semibold tw-pt-3 tw-text-[17px] text-center tw-text-gray-400">Published 1 week ago</p>
+            </div>
+            <div class="tw-px-6 lg:tw-px-0 lg:tw-pr-6">
+                <a href="/insights" class="tw-no-underline tw-text-xl">Back to Insights</a>
+                <div class="tw-text-5xl tw-py-4 tw-font-bold">The era of building a Terrace on main-land</div>
+                <div class=" tw-text-justify lg:tw-max-w-3xl md:tw-text-xl tw-text-[16px]">
+                    Lorem ipsum dolor sit amet consectetur. Arcu eu pellentesque mauris tincidunt
+                    in accumsan. Facilisis non pharetra ante convallis eleifend at molestie elementum
+                    Lorem ipsum dolor sit amet consectetur. Arcu eu pellentesque mauris tincidunt in
+                    accumsan. Facilisis non pharetra ante convallis eleifend at molestie elementum Lorem
+                    ipsum dolor sit amet consectetur. Arcu eu pellentesque mauris tincidunt in accumsan.
+                    Facilisis non pharetra ante convallis eleifend at molestie elementum Lorem ipsum dolor sit.
+                    accumsan. Facilisis non pharetra ante convallis eleifend at molestie elementum Lorem
+                    ipsum dolor sit amet consectetur. Arcu eu pellentesque mauris tincidunt in accumsan.
+                    Facilisis non pharetra ante convallis eleifend at molestie elementum Lorem ipsum dolorsit.
+                </div>
+                <div class=" tw-text-justify lg:tw-max-w-3xl md:tw-text-xl tw-text-[16px] py-4">
+                    Lorem ipsum dolor sit amet consectetur. Arcu eu pellentesque mauris tincidunt
+                    in accumsan. Facilisis non pharetra ante convallis eleifend at molestie elementum
+                    Lorem ipsum dolor sit amet consectetur. Arcu eu pellentesque mauris tincidunt in
+                    accumsan. Facilisis non pharetra ante convallis eleifend at molestie elementum Lorem
+                    ipsum dolor sit amet consectetur. Arcu eu pellentesque mauris tincidunt in accumsan.
+                    Facilisis non pharetra ante convallis eleifend at molestie elementum Lorem ipsum dolor sit.
+                    accumsan. Facilisis non pharetra ante convallis eleifend at molestie elementum Lorem
+                    ipsum dolor sit amet consectetur. Arcu eu pellentesque mauris tincidunt in accumsan.
+                    Facilisis non pharetra ante convallis eleifend at molestie elementum Lorem ipsum dolorsit.
                 </div>
             </div>
-            <div class="tw-grid tw-gap-4 lg:tw-grid-cols-3 md:tw-grid-cols-2 tw-grid-cols-1 tw-my-20">
-                <div class="tw-bg-white hover:tw-shadow-xl tw-transition-all tw-cursor-pointer tw-my-5">
-                    <div class=""><img src="img/post-1.png" alt="" class="tw-max-w-full"></div>
-                    <div class="tw-py-6 md:tw-px-8 tw-px-3">
-                        <div class="tw-text-gray-500">January, 21, 2023</div>
-                        <div class="tw-text-xl">Adron property on sales</div>
-                        <div class="">Adron is currently selling their property located at Lekki...</div>
-                        <div class="pt-2">
-                            <a href="" class="tw-no-underline">Read more</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="tw-bg-white hover:tw-shadow-xl tw-transition-all tw-cursor-pointer tw-my-5">
-                    <div class=""><img src="img/post-2.png" alt="" class="tw-max-w-full"></div>
-                    <div class="tw-py-6 md:tw-px-8 tw-px-3">
-                        <div class="tw-text-gray-500">January, 21, 2023</div>
-                        <div class="tw-text-xl">Adron property on sales</div>
-                        <div class="">Adron is currently selling their property located at Lekki...</div>
-                        <div class="pt-2">
-                            <a href="" class="tw-no-underline">Read more</a>
-                        </div>            </div>
-                </div>
-                <div class="tw-bg-white hover:tw-shadow-xl tw-transition-all tw-cursor-pointer tw-my-5">
-                    <div class=""><img src="img/post-3.png" alt="" class="tw-max-w-full"></div>
-                    <div class="tw-py-6 md:tw-px-8 tw-px-3">
-                        <div class="tw-text-gray-500">January, 21, 2023</div>
-                        <div class="tw-text-xl">Adron property on sales</div>
-                        <div class="">Adron is currently selling their property located at Lekki...</div>
-                        <div class="pt-2">
-                            <a href="" class="tw-no-underline">Read more</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="tw-bg-white hover:tw-shadow-xl tw-transition-all tw-cursor-pointer tw-my-5">
-                    <div class=""><img src="img/post-3.png" alt="" class="tw-max-w-full"></div>
-                    <div class="tw-py-6 md:tw-px-8 tw-px-3">
-                        <div class="tw-text-gray-500">January, 21, 2023</div>
-                        <div class="tw-text-xl">Adron property on sales</div>
-                        <div class="">Adron is currently selling their property located at Lekki...</div>
-                        <div class="pt-2">
-                            <a href="" class="tw-no-underline">Read more</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="tw-bg-white hover:tw-shadow-xl tw-transition-all tw-cursor-pointer tw-my-5">
-                    <div class=""><img src="img/post-2.png" alt="" class="tw-max-w-full"></div>
-                    <div class="tw-py-6 md:tw-px-8 tw-px-3">
-                        <div class="tw-text-gray-500">January, 21, 2023</div>
-                        <div class="tw-text-xl">Adron property on sales</div>
-                        <div class="">Adron is currently selling their property located at Lekki...</div>
-                        <div class="pt-2">
-                            <a href="" class="tw-no-underline">Read more</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="tw-bg-white hover:tw-shadow-xl tw-transition-all tw-cursor-pointer tw-my-5">
-                    <div class=""><img src="img/post-1.png" alt="" class="tw-max-w-full"></div>
-                    <div class="tw-py-6 md:tw-px-8 tw-px-3">
-                        <div class="tw-text-gray-500">January, 21, 2023</div>
-                        <div class="tw-text-xl">Adron property on sales</div>
-                        <div class="">Adron is currently selling their property located at Lekki...</div>
-                        <div class="pt-2">
-                            <a href="" class="tw-no-underline">Read more</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </article>
+        </div>
         <x-testimonials/>
     </div>
 @endsection
---
+++ resources/views/insights.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.public-layout')
+@section('content')
+    <x-big-banner>
+        <x-slot:head>
+            <div class="md:tw-text-6xl tw-text-4xl">insights</div>
+        </x-slot:head>
+    </x-big-banner>
+    <div class="tw-max-w-7xl mx-auto tw-py-16">
+        <article class="tw-mb-24 px-3">
+            <div class="tw-max-w-3xl mx-auto text-center">
+                <div class="c-header tw-font-black"> More insights</div>
+                <div class="c-border mx-auto"></div>
+                <div class="md:tw-text-xl tw-text-[15px] tw-text-gray-500">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Commodi unde impedit, necessitatibus, soluta sit quam minima
+                    expedita atque corrupti reiciendis.
+                </div>
+            </div>
+            <div class="tw-grid tw-gap-4 lg:tw-grid-cols-3 md:tw-grid-cols-2 tw-grid-cols-1 tw-my-20">
+                <div class="tw-bg-white hover:tw-shadow-xl tw-transition-all tw-cursor-pointer tw-my-5">
+                    <div class=""><img src="img/post-1.png" alt="" class="tw-max-w-full"></div>
+                    <div class="tw-py-6 md:tw-px-8 tw-px-3">
+                        <div class="tw-text-gray-500">January, 21, 2023</div>
+                        <div class="tw-text-xl">Adron property on sales</div>
+                        <div class="">Adron is currently selling their property located at Lekki...</div>
+                        <div class="pt-2">
+                            <a href="/insight" class="tw-no-underline">Read more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="tw-bg-white hover:tw-shadow-xl tw-transition-all tw-cursor-pointer tw-my-5">
+                    <div class=""><img src="img/post-2.png" alt="" class="tw-max-w-full"></div>
+                    <div class="tw-py-6 md:tw-px-8 tw-px-3">
+                        <div class="tw-text-gray-500">January, 21, 2023</div>
+                        <div class="tw-text-xl">Adron property on sales</div>
+                        <div class="">Adron is currently selling their property located at Lekki...</div>
+                        <div class="pt-2">
+                            <a href="" class="tw-no-underline">Read more</a>
+                        </div>            </div>
+                </div>
+                <div class="tw-bg-white hover:tw-shadow-xl tw-transition-all tw-cursor-pointer tw-my-5">
+                    <div class=""><img src="img/post-3.png" alt="" class="tw-max-w-full"></div>
+                    <div class="tw-py-6 md:tw-px-8 tw-px-3">
+                        <div class="tw-text-gray-500">January, 21, 2023</div>
+                        <div class="tw-text-xl">Adron property on sales</div>
+                        <div class="">Adron is currently selling their property located at Lekki...</div>
+                        <div class="pt-2">
+                            <a href="" class="tw-no-underline">Read more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="tw-bg-white hover:tw-shadow-xl tw-transition-all tw-cursor-pointer tw-my-5">
+                    <div class=""><img src="img/post-3.png" alt="" class="tw-max-w-full"></div>
+                    <div class="tw-py-6 md:tw-px-8 tw-px-3">
+                        <div class="tw-text-gray-500">January, 21, 2023</div>
+                        <div class="tw-text-xl">Adron property on sales</div>
+                        <div class="">Adron is currently selling their property located at Lekki...</div>
+                        <div class="pt-2">
+                            <a href="" class="tw-no-underline">Read more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="tw-bg-white hover:tw-shadow-xl tw-transition-all tw-cursor-pointer tw-my-5">
+                    <div class=""><img src="img/post-2.png" alt="" class="tw-max-w-full"></div>
+                    <div class="tw-py-6 md:tw-px-8 tw-px-3">
+                        <div class="tw-text-gray-500">January, 21, 2023</div>
+                        <div class="tw-text-xl">Adron property on sales</div>
+                        <div class="">Adron is currently selling their property located at Lekki...</div>
+                        <div class="pt-2">
+                            <a href="" class="tw-no-underline">Read more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="tw-bg-white hover:tw-shadow-xl tw-transition-all tw-cursor-pointer tw-my-5">
+                    <div class=""><img src="img/post-1.png" alt="" class="tw-max-w-full"></div>
+                    <div class="tw-py-6 md:tw-px-8 tw-px-3">
+                        <div class="tw-text-gray-500">January, 21, 2023</div>
+                        <div class="tw-text-xl">Adron property on sales</div>
+                        <div class="">Adron is currently selling their property located at Lekki...</div>
+                        <div class="pt-2">
+                            <a href="" class="tw-no-underline">Read more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </article>
+        <x-testimonials/>
+    </div>
+@endsection
