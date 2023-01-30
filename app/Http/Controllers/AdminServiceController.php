8a40695f10dec37b6212be5c75e5f057671c2a70
Abayomi Aluko <franicons@gmail.com>
2023-01-30 11:15:50 +0100

    feat: Working on backend

--- app/Http/Controllers/AdminServiceController.php
+++ app/Http/Controllers/AdminServiceController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Service;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class AdminServiceController extends Controller {
 
@@ -11,5 +12,23 @@ class AdminServiceController extends Controller {
         return view('admin.services.index', [
             'services' => Service::paginate(6)
         ]);
+    }
+
+    public function create() {
+        return view('admin.services.create');
+    }
+
+    public function store() {
+        $attributes = request()->validate([
+            'title' => 'required',
+            'image' => 'required',
+            'body' => 'required'
+        ]);
+
+        $attributes['image'] = Storage::disk('public')->putFile('/public/img', request()->file('image'));
+
+        Service::create($attributes);
+
+        return back()->with('success', 'Service Created successfully');
     }
 }
--- resources/views/admin/services/create.blade.php
+++ resources/views/admin/services/create.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class=" mx-auto">
         <section class="container tw-py-10">
-            <form class="row justify-content-center g-3 mt-5" method="POST" action="/admin/insights" enctype="multipart/form-data">
+            <form class="row justify-content-center g-3 mt-5" method="POST" action="/admin/services" enctype="multipart/form-data">
                 @csrf
                 <div class="col-12">
                     <h3 class="tw-font-bold">Publish New Services</h3>
@@ -11,29 +11,29 @@
                 <div class="col-md-2">
                     <div class="tw-text-xl tw-mb-6 tw-font-bold">Links</div>
                     <div class="tw-pb-3">
-                        <a  class="{{request()->is('admin/insights') ? 'py-2 tw-font-semibold tw-text-[17px] tw-no-underline' : 'tw-no-underline tw-text-[17px] tw-text-stone-800'}}" href="/admin/insights">All Services</a>
+                        <a  class="{{request()->is('admin/services') ? 'py-2 tw-font-semibold tw-text-[17px] tw-no-underline' : 'tw-no-underline tw-text-[17px] tw-text-stone-800'}}" href="/admin/services">All Services</a>
                     </div>
                     <div class="tw-pb-3">
-                        <a  class="{{request()->is('admin/insights/create') ? 'tw-font-semibold tw-text-[17px] py-2 tw-no-underline' : 'tw-no-underline tw-text-[17px] tw-text-stone-800'}}"
-                            href="/admin/insights/create">New Service</a>
+                        <a  class="{{request()->is('admin/services/create') ? 'tw-font-semibold tw-text-[17px] py-2 tw-no-underline' : 'tw-no-underline tw-text-[17px] tw-text-stone-800'}}"
+                            href="/admin/services/create">New Service</a>
                     </div>
                 </div>
                 <div class="col-md-10 tw-border-[1.5px] tw-rounded-3xl py-4">
                     <div class="px-3">
                         <div class="form-floating mb-4">
                             <input type="text" name="title" required class="form-control" id="title" placeholder="title">
-                            <label for="title">Title: E.g - Design</label>
+                            <label for="title">Title: E.g - Design and build</label>
                             @error('title')
                             <p class="text-danger tw-py-4">{{$message}}</p>
                             @enderror
                         </div>
                         <div class="input-group mb-4">
-                            <label class="input-group-text" for="thumbnail">Upload Image</label>
-                            <input type="file" name="thumbnail" class="form-control" id="inputGroupFile01">
+                            <label class="input-group-text" for="image">Upload Image</label>
+                            <input type="file" name="image" class="form-control" id="inputGroupFile01">
                         </div>
                         <div class="form-floating mb-4">
                             <textarea class="form-control" name="body" placeholder="body" id="body" style="height: 200px"></textarea>
-                            <label for="body">Body</label>
+                            <label for="body">Write Description</label>
                             @error('body')
                             <p class="text-danger tw-py-4">{{$message}}</p>
                             @enderror
