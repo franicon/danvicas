3233fa85cb7fe00fefafcd542a8f70b255d5ec04
Abayomi Aluko <franicons@gmail.com>
2023-01-31 13:59:27 +0100

    feat: Working on backend

--- resources/views/quotation.blade.php
+++ resources/views/quotation.blade.php
@@ -8,25 +8,17 @@
     <section class="container tw-py-10">
         <form class="row g-3 mt-5 justify-content-center">
             <div class="col-md-6">
-                <div class="px-3">
-                    <div class="form-floating mb-2">
-                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                        <label for="floatingInput">Email address</label>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
                 <div class="px-3 row">
                     <div class="col-md-6">
                         <div class="form-floating mb-2">
                             <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                            <label for="floatingInput">Email address</label>
+                            <label for="floatingInput">First Name</label>
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-floating mb-2">
                             <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                            <label for="floatingInput">Email address</label>
+                            <label for="floatingInput">Last Name</label>
                         </div>
                     </div>
                 </div>
@@ -43,7 +35,15 @@
                 <div class="px-3">
                     <div class="form-floating mb-2">
                         <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                        <label for="floatingInput">Email address</label>
+                        <label for="floatingInput">Phone Number</label>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="px-3">
+                    <div class="form-floating mb-2">
+                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                        <label for="floatingInput">Project Title</label>
                     </div>
                 </div>
             </div>
@@ -51,7 +51,7 @@
                 <div class="px-3">
                     <div class="form-floating">
                         <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
-                        <label for="floatingTextarea2">Message</label>
+                        <label for="floatingTextarea2">About your company</label>
                     </div>
                 </div>
             </div>
@@ -59,7 +59,7 @@
                 <div class="px-3">
                     <div class="form-floating">
                         <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
-                        <label for="floatingTextarea2">Message</label>
+                        <label for="floatingTextarea2">More info about project</label>
                     </div>
                 </div>
             </div>
