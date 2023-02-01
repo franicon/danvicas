e78e5f2314319b9ddd75ffdc513e901781c0576e
Abayomi Aluko <franicons@gmail.com>
2023-02-01 13:33:10 +0100

    feat: Working on backend

---
+++ app/Http/Controllers/AdminTestimonialController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Testimonial;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class AdminTestimonialController extends Controller {
+    public function index() {
+        return view('admin.testimonials.index', [
+            'testimonials' => Testimonial::paginate(6)
+        ]);
+    }
+
+    public function create() {
+        return view('admin.testimonials.create');
+    }
+
+    public function edit(Testimonial $testimonial) {
+        return view('admin.testimonials.edit', ['testimonial' => $testimonial]);
+    }
+
+    public function store() {
+        $attributes = request()->validate([
+            'name' => 'required',
+            'image' => 'required',
+            'body' => 'required'
+        ]);
+
+        $attributes['image'] = Storage::disk('public')->putFile('/public/img', request()->file('image'));
+
+        Testimonial::create($attributes);
+
+        return redirect('/admin/testimonials')->with('success', 'Testimonial Created successfully');
+    }
+
+    public function update(Testimonial $testimonial) {
+        $attributes = request()->validate([
+            'name' => 'required',
+            'image' => 'image',
+            'body' => 'required'
+        ]);
+
+        if (isset($attributes['image'])) {
+            $attributes['image'] = Storage::disk('public')->putFile('/public/img', request()->file('image'));
+        }
+        $testimonial->update($attributes);
+
+        return redirect('/admin/testimonials')->with('success', 'Testimonial Updated Successfully');
+    }
+
+    public function destroy(Testimonial $testimonial) {
+        $testimonial->delete();
+
+        return back()->with('success', 'Testimonial Deleted successfully');
+    }
+}
--- app/Http/Controllers/TestimonialController.php
+++ app/Http/Controllers/TestimonialController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Testimonial;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
-class TestimonialController extends Controller
-{
-    //
+class TestimonialController extends Controller {
+    public function index() {
+        return view('admin.testimonials.index', [
+            'testimonials' => Testimonial::paginate(6)
+        ]);
+    }
+
 }
