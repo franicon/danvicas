9024eb1165e61c59d55cbd8b5d5129a4b6c1d8c8
Abayomi Aluko <franicons@gmail.com>
2023-02-01 13:32:59 +0100

    feat: Working on backend

--- database/migrations/2023_02_01_120550_create_testimonials_table.php
+++ database/migrations/2023_02_01_120550_create_testimonials_table.php
@@ -1,31 +1,21 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
+return new class extends Migration {
+    public function up() {
         Schema::create('testimonials', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->longText('body');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('testimonials');
     }
 };
